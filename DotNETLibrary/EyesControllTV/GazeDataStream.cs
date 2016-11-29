615a22369217744aba409d13f688a45eacb1e3fa
ziyousong <andy900508@gmail.com>
2016-11-29 20:24:50 +0800

    Update GazeDataStream.cs

--- DotNETLibrary/EyesControllTV/GazeDataStream.cs
+++ DotNETLibrary/EyesControllTV/GazeDataStream.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using EyeXFramework;
-using Tobii.EyeX.Framework;
-using EyesControllTV;
 using System.Windows;
 
 namespace EyesControllTV
