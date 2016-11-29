bcd452c01e4dfe557ecd52d04492de66b41bd5ee
ziyousong <andy900508@gmail.com>
2016-11-29 20:17:53 +0800

    Add files via upload

---
+++ DotNETLibrary/EyesControllTV/ArduinoConnection.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+using EyesControllTV;
+
+namespace EyesControllTV
+{
+    public class ArduinoConnection
+    {
+        public static SerialPort port;
+        //public string PortName { get; set; }
+        //public int BaudRate { get; set; }
+        public DateTime eyeTime = DateTime.Now;
+
+        private bool inblock = false;
+
+        public ArduinoConnection(string PortName, int BaudRate)
+        {
+            try
+            {
+                port = new SerialPort();
+                port.BaudRate = BaudRate;
+                port.PortName = PortName;
+                port.Open();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Error");
+            }
+        }
+
+        public void PowerON(int seconds)
+        {
+            if (inblock)
+            {
+                if((DateTime.Now - eyeTime).TotalMilliseconds > seconds)
+                {
+                    inblock = false;
+                    eyeTime = DateTime.Now;
+                    port.WriteLine("1");
+                }
+            }
+            else
+            {
+                inblock = true;
+                eyeTime = DateTime.Now;
+            }
+        }
+
+        public void NextChannel(int seconds)
+        {
+            if (inblock)
+            {
+                if ((DateTime.Now - eyeTime).TotalMilliseconds > seconds)
+                {
+                    inblock = false;
+                    eyeTime = DateTime.Now;
+                    port.WriteLine("2");
+                }
+            }
+            else
+            {
+                inblock = true;
+                eyeTime = DateTime.Now;
+            }
+        }
+
+        public void PrevChannel(int seconds)
+        {
+            if (inblock)
+            {
+                if ((DateTime.Now - eyeTime).TotalMilliseconds > seconds)
+                {
+                    inblock = false;
+                    eyeTime = DateTime.Now;
+                    port.WriteLine("3");
+                }
+            }
+            else
+            {
+                inblock = true;
+                eyeTime = DateTime.Now;
+            }
+        }
+
+    }
+}
---
+++ DotNETLibrary/EyesControllTV/GazeDataStream.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EyeXFramework;
+using Tobii.EyeX.Framework;
+using EyesControllTV;
+using System.Windows;
+
+namespace EyesControllTV
+{
+    public class GazeDataStream
+    {
+        public double eyeX { get; set; }
+        public double eyeY { get; set; }
+        private static double Sheight, Swidth, DivHeight, DivWidth;
+
+        private EyeXHost _eyeXHost = new EyeXHost();
+        GazePointDataStream stream;
+
+        public GazeDataStream()
+        {
+            Sheight = SystemParameters.PrimaryScreenHeight;
+            Swidth = SystemParameters.PrimaryScreenWidth;
+            DivHeight = Sheight / 10;
+            DivWidth = Swidth / 10;
+        }
+
+        public void startEyeTrack()
+        {
+            stream = _eyeXHost.CreateGazePointDataStream(Tobii.EyeX.Framework.GazePointDataMode.LightlyFiltered);
+            stream.Next += new EventHandler<GazePointEventArgs>(getEyeData);
+            _eyeXHost.Start();
+        }
+
+        private void getEyeData(object sender, GazePointEventArgs e)
+        {
+            eyeX = Math.Round(e.X / DivWidth, 0);
+            eyeY = Math.Round(e.Y / DivHeight, 0);
+        }
+
+        public double getEyeX()
+        {
+            return eyeX;
+        }
+
+        public double getEyeY()
+        {
+            return eyeY;
+        }
+
+    }
+}
